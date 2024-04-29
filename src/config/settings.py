bd6e2fa6cf194e13a645dc7d76760540708120ac
Vladislav Yaremenko <vlad.yaremenko.98@yandex.ru>
2024-04-29 08:04:27 +0300

    Remove link to site in admin panel

--- src/config/settings.py
+++ src/config/settings.py
@@ -62,7 +62,7 @@ AUTH_USER_MODEL = 'users.User'
 TEMPLATES = (
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': (os.path.join(BASE_DIR, 'templates'),),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': (
